fa3002b1cdb5716a4c543e7d2ed806db0811a69a
Joao <jfjoao12@gmail.com>
2025-09-09 10:50:56 -0500

    Class 114: Handling not found pages

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("063e162a9a14f0d6")
+/******/ 		__webpack_require__.h = () => ("3ca7b09895691b06")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "20e4e905708adc8a"; }
+/******/ 		__webpack_require__.h = function() { return "6450b3f07df38c1a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
